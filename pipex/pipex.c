5cdde167f64205b28e48b87f6f1ff4b37951f584
namejojo <jlima-so@42lisboa.com>
2025-06-08 01:19:08 +0100

    msg

--- pipex/pipex.c
+++ pipex/pipex.c
@@ -6,7 +6,7 @@
 /*   By: jlima-so <jlima-so@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/05 08:19:15 by jlima-so          #+#    #+#             */
-/*   Updated: 2025/06/07 17:25:14 by jlima-so         ###   ########.fr       */
+/*   Updated: 2025/06/08 01:18:11 by jlima-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@
 #include <stdlib.h>
 #include "my_libft/libft.h"
 
-// maybe add??
+// maybe add get_file??
 
 char	*get_file(int fd)
 {
@@ -37,28 +37,81 @@ char	*get_file(int fd)
 	return (str);
 }
 
+// maybe add get_file??
+
+void	rdwr_frm_int_fd(char *av, char **env, int *rd, int wr)
+{
+	t_exec	ret;
+	char	**mat;
+	char	*str;
+	int		ind;
+
+	dup2 (rd[0], STDIN_FILENO);
+	close (rd[0]);
+	dup2 (wr, STDOUT_FILENO);
+	close (wr);
+	ind = -1;
+	ret.cmd = ft_split(av, ' ');
+	str = ft_strjoin("/", ret.cmd[0]);
+	mat = ft_split(ft_strmat(env, "PATH=") + 5, ':');
+	while (mat[++ind])
+	{
+		ret.path = ft_strjoin(mat[ind], str);
+		execve(ret.path, ret.cmd, env);
+		free (ret.path);
+	}
+	ft_free_matrix(mat);
+	ft_free_matrix(ret.cmd);
+	free (str);
+	exit (0);
+}
+
+int	feed_file_into_pipe(char **av, char **env, int *fd2)
+{
+	int		fd;
+	int		fd1[2];
+	char	*str;
+	int		id;
+
+	fd = open(av[1], O_RDONLY);
+	if (fd < 0)
+		return (perror(strerror(errno)), errno);
+	str = get_file(fd);
+	close (fd);
+	if (pipe(fd1))
+		return (perror(strerror(errno)), errno);
+	ft_putstr_fd(str, fd1[1]);
+	free (str);
+	close (fd1[1]);
+	if (pipe(fd2))
+		return (perror(strerror(errno)), errno);
+	id = fork();
+	if (id < 0)
+		return (perror(strerror(errno)), errno);
+	if (id == 0)
+		rdwr_frm_int_fd (av[2], env, fd1, fd2[1]);
+	close (fd1[0]);
+	waitpid(id, NULL, 0);
+	return (0);
+}
+
 int main(int ac, char **av, char **env)
 {
 	char	*str;
-	int		sfd;
-	int		fd[2];
+	int		fd;
 	int		fd2[2];
+	int		id;
 
-	sfd = open(av[1], O_RDONLY);
-	if (sfd < 0)
+	if (ac < 5)
+		return (write (2, "invalid number of argumants\n", 29));
+	if (feed_file_into_pipe(av, env, fd2))
 		return (perror(strerror(errno)), errno);
-	str = get_file(sfd);
-	close (sfd);
-	if (pipe(fd))
-		return (perror(strerror(errno)), errno);
-	ft_putstr_fd(str, fd[1]);
-	if (pipe(fd2))
-		return (perror(strerror(errno)), errno);
-	rd_frm_pipe_wr_int_pipe(fd[0], fd2[1]);
-	sfd = open(av[ac - 1], O_WRONLY);
-	if (sfd < 0)
-		return (perror(strerror(errno)), errno);
-	rd_frm_pipe_wr_int_pipe(fd2[0], sfd);
-	close (sfd);
-	free (str);
+	fd = open (av[4], O_WRONLY);
+	id = fork ();
+	close (fd2[1]);
+	if (id == 0)
+		rdwr_frm_int_fd (av[3], env, fd2, fd);
+	close (fd2[0]);
+	waitpid (id, NULL, 0);
+	return (0);
 }
